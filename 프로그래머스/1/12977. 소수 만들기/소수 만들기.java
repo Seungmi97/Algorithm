79b6f8f165b59c7146bad9084706f3e35284595b
Seungmi97 <132995507+Seungmi97@users.noreply.github.com>
2025-03-06 05:12:23 +0900

    [level 1] Title: 소수 만들기, Time: 73.40 ms, Memory: 99.6 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -2,25 +2,25 @@ class Solution {
     public int solution(int[] nums) {
         int answer = 0;
 
-        answer = sum(nums, 0, 0, 0, 0, answer);
+        answer = countPrime(nums, 0, 0, 0, 0, answer);
 
         return answer;
     }
 
-    int sum(int[] nums, int res, int start, int index, int depth, int count) {
-        res += nums[index];
+    int countPrime(int[] nums, int sum, int start_index, int index, int depth, int count) {
+        sum += nums[index];
 
         while (depth < 2){
             if (++index < nums.length) {
-                count = sum(nums, res, start, index, ++depth, count);
+                count = countPrime(nums, sum, start_index, index, ++depth, count);
                 depth--;
                 if (depth == -1) {
                     return count;
                 }
             } else if (depth == 0) {
-                if (start < nums.length - 3) {
-                    index = start++;
-                    res = 0;
+                if (start_index < nums.length - 3) {
+                    index = start_index++;
+                    sum = 0;
                     depth = -1;
                 } else {
                     return count;
@@ -29,7 +29,7 @@ class Solution {
                 return count;
             }
         }
-        if (isPrimeNumber(res)) {
+        if (isPrimeNumber(sum)) {
             count++;
         }
 
